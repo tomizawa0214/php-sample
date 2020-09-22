6876889b42c93fa0d5ff8897c488c5851f45954b
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-09-22 19:13:52 +0900

    second-commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $name = 'にんじゃりばんばん';
+        echo $name;
+      ?>
+    </p>
+  </body>
+</html>
